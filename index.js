6386bb6b4a8d35b4c014a8800bf194c2886d50b7
zijjj <renjie@zijjj.com>
2017-10-31 17:58:02 +0800

    1

---
+++ index.js
@@ -0,0 +1,47 @@
+const Vue = require("vue");
+const ElementUi = require("element-ui");
+
+Vue.use(ElementUi);
+const vm = new Vue;
+
+// 格式化时间
+// 返回结果 2017/07/08 08:05:10
+const formatTime = date => {
+  const year = date.getFullYear()
+  const month = date.getMonth() + 1
+  const day = date.getDate()
+  const hour = date.getHours()
+  const minute = date.getMinutes()
+  const second = date.getSeconds()
+
+  return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
+}
+
+const formatNumber = n => {
+  n = n.toString()
+  return n[1] ? n : '0' + n
+}
+
+
+
+//合并对象属性
+function merge(target) {
+  for (let i = 1, j = arguments.length; i < j; i++) {
+    let source = arguments[i] || {};
+    for (let prop in source) {
+      if (source.hasOwnProperty(prop)) {
+        let value = source[prop];
+        if (value !== undefined) {
+          target[prop] = value;
+        }
+      }
+    }
+  }
+  return target;
+};
+
+
+module.exports = {
+  formatTime,
+  merge
+}
